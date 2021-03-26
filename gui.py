89c49b673819f964caf8b1a28463cb9acb082cc3
maurice <m_partee@hotmail.com>
2021-03-25 23:04:54 -0500

    Basic preview pane. Currently holds image but will hold camera preview

--- gui.py
+++ gui.py
@@ -1,16 +1,20 @@
 from tkinter import *
+from PIL import ImageTk,Image
 
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
-
-# initialize tkinter
 root = Tk()
-app = Window(root)
+root.title("Moe's PhotogrammePi")
 
-# set window title
-root.wm_title("Moe's Photogrammetry")
 
-# show window
+#Define preview pane in Root
+frame=LabelFrame(root, text="Preview Pane", padx=50, pady=50)
+#Create Preview Pane in root
+frame.pack(padx=10, pady=10)
+
+#Create Image
+img = ImageTk.PhotoImage(Image.open('images/download.jpeg'))
+#define location and pane for image
+label = Label(frame, image=img)
+#Create image location
+label.pack()
+
 root.mainloop()
